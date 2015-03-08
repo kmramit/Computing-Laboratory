23b180beb1a6892798b7c2d2f42d364935ca7fb6
kmramit <amiitkumar1995@gmail.com>
2015-03-08 23:33:56 +0530

    updated version of clock in HH:MM:SS format

--- assignment4/clock.sh
+++ assignment4/clock.sh
@@ -1,16 +1,26 @@
 while true
 do
-	for i in `seq 0 1 23`
-	do
-		for j in `seq 0 1 59`
-		do
-			for k in `seq 0 1 59`
-			do
-				clear
-				banner $i:$j:$k
-				sleep 1
-			done
-		done
-	done
+	ans=$(date +"%T")
+	clear
+	banner $ans
+	sleep 1
 done
+
+
+
+#while true
+#do
+#	for i in `seq 0 1 23`
+#	do
+#		for j in `seq 0 1 59`
+#		do
+#			for k in `seq 0 1 59`
+#			do
+#				clear
+#				banner $i:$j:$k
+#				sleep 1
+#			done
+#		done
+#	done
+#done
 
