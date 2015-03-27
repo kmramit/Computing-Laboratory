d1191fc1367513f9bbcf31e1c56c0ca7b061e144
kmramit <amiitkumar1995@gmail.com>
2015-03-27 14:52:37 +0530

    octave script for creating 1000,10^4,10^5,10^6 sample data for gaussian distribution has been added

---
+++ assignment5/gaussian/generate.m
@@ -0,0 +1,13 @@
+a= normrnd(15,sqrt(10.5),1000,1);
+save gau1 a;
+
+b= normrnd(15,sqrt(10.5),10000,1);
+save gau2 b;
+
+c= normrnd(15,sqrt(10.5),100000,1);
+save gau3 c;
+
+d= normrnd(15,sqrt(10.5),1000000,1);
+save gau4 d;
+
+
